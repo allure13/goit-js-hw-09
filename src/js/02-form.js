f37efdebd5fc11375fbbb02aac4d9dd080806b83
allure13 <igor.cheplyaka@gmail.com>
2023-12-17 18:03:29 +0200

    12312

---
+++ src/js/02-form.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.querySelector('.feedback-form');
+
+  form.addEventListener('input', function (event) {
+    const formData = {
+      email: form.elements.email.value,
+      message: form.elements.message.value,
+    };
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  });
+
+  const savedFormData = localStorage.getItem('feedback-form-state');
+
+  if (savedFormData) {
+    const parsedData = JSON.parse(savedFormData);
+    form.elements.email.value = parsedData.email;
+    form.elements.message.value = parsedData.message;
+  }
+
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    localStorage.removeItem('feedback-form-state');
+    form.reset();
+
+    const formData = {
+      email: form.elements.email.value,
+      message: form.elements.message.value,
+    };
+    console.log(formData);
+  });
+});
